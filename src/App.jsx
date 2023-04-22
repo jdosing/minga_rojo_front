ad62f0656c2b183e715dc8ac2b8994a2ea10a381
jdosing <96783149+jdosing@users.noreply.github.com>
2023-04-22 14:40:32 -0500

    se monta fuera de rama f-01

--- src/App.jsx
+++ src/App.jsx
@@ -5,26 +5,111 @@ import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
-
+  // text-white pl-16 pt-24 
   return (
     <>
-    <body className='w-[100%]'>
-      <header className=''>
-      <nav className=' border-solid border-2 border-indigo-600 max-w-screen-2xl'>
-          <div className='h-96 border-solid border-2 border-indigo-600 bg-no-repeat bg-cover bg-[url(/src/assets/images/Imagesbannerhome.png)]'> 
+    <body className='xsm:w-[100vw] md:w-[100vw] xh:w-[100vw] '>
+      <header className='xsm:w-[100vw] md:w-[100vw] xh:w-[100vw]'>
+        <nav>
+        <div className='xsm:w-[100vw] xsm:h-[100vh] md:w-[100vw] md:h-[70vh] md:border-solid border-2 bg-no-repeat bg-cover bg-[url(/src/assets/images/Imagesbannerhome.png)] xh:w-[100vw] xh:h-[70vh]'> 
             <div className='flex justify-between mt-4'>
               <img className='pl-12' src="src/assets/images/Menu.png" alt="" />
-              <img className='pr-9' src="src/assets/images/Logo.png" alt="" />
+              <div className='flex'>
+                <img className='xsm:hidden md:pr-9' src="src/assets/images/Minga.png" alt=""/>
+              <img className='xsm:pr-7 md:pr-9' src="src/assets/images/雪.png" alt="" />
+              </div>
             </div>
-            <div className='text-white pl-16 pt-24' >
-              <h1 className='text-5xl font-bold'>For the love of manga</h1>
-              <h3 className='font-light'>Explore our varieties</h3>
-              <h5 className='font-thin'>#MingaLove❤</h5>
+            <div className='xsm:text-center xsm:my-42 xsm:text-white xsm:my-[14rem]  md:text-white md:pl-16 md:flex flex-col md:pt-28  ' >
+              <h1 className='xsm:text-white xsm:pb-2 text-5xl font-bold '>For the love of manga</h1>
+              <h3 className='xsm:text-white xsm:pb-3 font-extralight'>Explore our varieties</h3>
+              <h5 className='xsm:hidden md:font-normal'>#MingaLove❤</h5>
+              <div className='xsm:flex justify-center'>
+                <img className='xsm:w-[40vw] xsm: md:' src="/src/assets/images/botonmovil.png" alt="" />
+              </div>
+              
+            </div>
+          </div>      
+        </nav>
+      </header>
+      {/* <main className='xsm:w-[100vw] md:w-[100vw] '> */}
+      <main className='xsm:hidden md:h-96 flex justify-center items-center border-solid border-2 max-w-screen-2xl xh:flex xh:justify-center xh:items-center xh:border-solid xh:border-2 xh:max-w-screen-2xl '>        
+          <div className='justify-center flex bg-gradient-to-r rounded-md from-orange-400 to-orange-600 h-[80%] w-[90%]'>
+            <div className='absolute w-[88%] flex content-center my-28 justify-between ' >
+              <img src="/src/assets/images/flechaderecha.png" alt="/" />
+              <img src="/src/assets/images/flechaizquierda.png" alt="" />
+            </div>
+            <div className='flex h-[50%] w-[50%] ml-10'>
+                <div className='h-[200%] w-[160%]'>
+                    <img className='md:h-[110%] md:w-[100%] xh:w-[80%]  -my-5' src="/src/assets/images/imgmain1.png" alt="" />
+                </div>
+                <div className='h-[140%] w-[135%] ml-10 -my-5'>
+                    <img  className='md:h-[150%] md:w-[110%] xh:w-[80%]' src="/src/assets/images/imgmain2.png" alt="" />
+                </div>
+            </div>
+            <div className='h-[60%] w-[55%] mx-10 my-10'>
+              <h3 className='font-bold text-l'>
+              Shonen
+              </h3>
+              <p className='text-l h-[90%] w-[80%] '>
+              Is the manga that is aimed at adolescent boys. They are series with large amounts of action, in which humorous situations often occur. The camaraderie between members of a collective or a combat team stands out.
+              </p>
+            </div>
+          </div>
+      </main>
+      {/* </main> */}
+      <footer className='xsm:hidden max-w-screen-2xl'>
+        <div className='max-w-screen-2xl'>
+          <img className='w-[100%]' src="/src/assets/images/imgfooter.png" alt="" />
+        </div>
+        <div className='flex justify-around'>
+          <div className=''>
+            <a className='mr-2' href="">Home</a>
+            <a className='ml-2' href="">Mangas</a>
+          </div>
+          <div className='h-[10%] flex'>
+            <img src="src/assets/images/Minga.png" alt="" />
+            <img src="src/assets/images/雪.png" alt="" />
+          </div>
+          <div>
+            <div className='flex justify-between'>
+              <img src="src/assets/images/facebook-black.png" alt="" />
+              <img src="src/assets/images/twitter-black.png" alt="" />
+              <img src="src/assets/images/vimeo-black.png" alt="" />
+              <img src="src/assets/images/youtube-black.png" alt="" />
+            </div>
+            <div className=''>
+              <img src="src/assets/images/donate.png" alt="" />
+            </div>
+          </div>
+        </div>
+        <div className='m-10 border-2 border-grey-400'>
+        
+        </div>
+      </footer>
+    </body>
+    {/* <body className='xsm:w-[100vw] md:w-[10vw] '>
+      <header className='xsm:w-[100vw] md:w-[10vw] '>
+      <nav className='flex justify-center xsm:h-screen md:h-screen '>
+          <div className='xsm:w-full xsm:h-full md:w-full md:h-[90vh] md:border-solid border-2 bg-no-repeat bg-cover bg-[url(/src/assets/images/Imagesbannerhome.png)]'> 
+            <div className='flex justify-between mt-4'>
+              <img className='pl-12' src="src/assets/images/Menu.png" alt="" />
+              <div className='flex'>
+              <img className='xsm:hidden md:pr-9' src="src/assets/images/Minga.png" alt="" />
+              <img className='xsm:pr-7 md:pr-9' src="src/assets/images/雪.png" alt="" />
+              </div>
+            </div>
+            <div className='xsm:flex xsm:text-center xsm:flex-col xsm:text-white pl-1 pt-24  md:text-white md:pl-16 md:pt-24 xsm:my-32' >
+              <h1 className='xsm:text-white text-5xl font-bold'>For the love of manga</h1>
+              <h3 className='xsm:text-white font-extralight'>Explore our varieties</h3>
+              <div className='flex justify-center'>
+                <img className='xsm:w-[40vw] xsm: md:hidden' src="/src/assets/images/botonmovil.png" alt="" />
+              </div>
+              <h5 className='xsm:hidden md:font-thin'>#MingaLove❤</h5>
             </div>
           </div>      
       </nav>
     </header>
-      <main className='h-96 flex justify-center items-center border-solid border-2 border-indigo-600 max-w-screen-2xl '>        
+      <main className='xsm:hidden md:h-96 flex justify-center items-center border-solid border-2 max-w-screen-2xl '>        
           <div className='justify-center flex bg-gradient-to-r rounded-md from-orange-400 to-orange-600 h-[70%] w-[90%]'>
             <div className='absolute w-[88%] flex content-center my-28 justify-between ' >
               <img src="/src/assets/images/flechaderecha.png" alt="/" />
@@ -48,7 +133,7 @@ function App() {
             </div>
           </div>
       </main>
-      <footer className='max-w-screen-2xl'>
+      <footer className='xsm:hidden max-w-screen-2xl'>
         <div className='max-w-screen-2xl'>
           <img className='w-[100%]' src="/src/assets/images/imgfooter.png" alt="" />
         </div>
@@ -68,7 +153,7 @@ function App() {
               <img src="src/assets/images/vimeo-black.png" alt="" />
               <img src="src/assets/images/youtube-black.png" alt="" />
             </div>
-            <div>
+            <div className=''>
               <img src="src/assets/images/donate.png" alt="" />
             </div>
           </div>
@@ -78,7 +163,7 @@ function App() {
         </div>
       </footer>
     </body>
-    
+     */}
     </>
   )
 }
