2ad1de2eb6e35b864d6a9246da2312c8e8121c72
jdosing <96783149+jdosing@users.noreply.github.com>
2023-04-24 18:39:40 -0500

    M 02 (#9)
    * corrige
    
    * correccion scroll
    
    * corregir hecho

--- src/App.jsx
+++ src/App.jsx
@@ -10,11 +10,14 @@ function App() {
   console.log(process.env.NODE_ENV);
   // text-white pl-16 pt-24 
   return (
+    
     <>
+
     <div className='xsm:w-[100vw] md:w-[100vw] xh:w-[100vw] '>
       <header className='xsm:w-[100vw] md:w-[100vw] xh:w-[100vw]'>
         <nav>
         <div className='xsm:w-[100vw] xsm:h-[100vh] md:w-[100vw] md:h-[50vh] md:border-solid border-2 bg-no-repeat bg-cover bg-[url(/images/Imagesbannerhome.png)] xh:w-[99vw] xh:h-[70vh]'> 
+
             <div className='flex justify-between mt-4'>
               <img className='pl-12' src="/images/Menu.png" alt="" />
               <div className='flex'>
@@ -22,7 +25,9 @@ function App() {
               <img className='xsm:pr-7 md:pr-9' src="/images/雪.png" alt="" />
               </div>
             </div>
-            <div className='xsm:text-center xsm:text-white xsm:my-[10rem]  md:text-white md:pl-16 md:flex flex-col md:pt-10  ' >
+
+            <div className='xsm:text-center xsm:text-white xsm:my-[10rem]  md:text-white md: pl-10 md:flex flex-col md:pt-10  ' >
+
               <h1 className='xsm:text-white xsm:pb-2 text-5xl font-bold '>For the love of manga</h1>
               <h3 className='xsm:text-white xsm:pb-3 font-extralight'>Explore our varieties</h3>
               <h5 className='xsm:hidden md:font-normal'>#MingaLove❤</h5>
